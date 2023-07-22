9ad0c60e1a00e1416b6e911b729788c5d78a0c2a
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2023-07-22 22:38:08 +0300

    Brands CRUD Operations

--- server.js
+++ server.js
@@ -2,59 +2,56 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
+dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/database");
+const ApiError = require("./utils/apiError");
+const globalError = require("./middlewares/errorMiddleware");
 
-dotenv.config({ path: "config.env" })
-const dbConnection = require("./config/database")
-const ApiError = require("./utils/apiError")
-const globalError = require("./middlewares/errorMiddleware")
-const categoryRoute = require("./routes/categoryRoute")
-const subcategoryRoute = require("./routes/subcategoryRoute")
+// Routes
+const categoryRoute = require("./routes/categoryRoute");
+const subcategoryRoute = require("./routes/subcategoryRoute");
+const brandRoute = require("./routes/brandRoute");
 
 // ------------------------------------------- Connect with DB
 dbConnection();
 
-// ------------------------------------------- express 
+// ------------------------------------------- express
 const app = express();
 
-// ------------------------------------------- middlewares 
+// ------------------------------------------- middlewares
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'))
-    console.log(`mode: ${process.env.NODE_ENV}`)
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
 // ------------------------------------------- Mount Route
 app.use("/api/v1/categories", categoryRoute);
 app.use("/api/v1/subcategories", subcategoryRoute);
+app.use("/api/v1/brands", brandRoute);
 
 app.all("*", (req, res, next) => {
-    next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400))
-})
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+});
 
 // ------------------------------------------- Global error handling middleware for express
 app.use(globalError);
 
 // ------------------------------------------- Server listen
-const {PORT} = process.env;
+const { PORT } = process.env;
 const server = app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
-})
-
+  console.log(`Server running on port: ${PORT}`);
+});
 
 // Event => list => callback(err)
 process.on("unhandledRejection", (err) => {
-    console.error(`unhandledRejection Error: ${err.name} | ${err.message}`);
-    server.close(() => {
-        console.error("Shutting down...");
-        process.exit(1);
-    });
+  console.error(`unhandledRejection Error: ${err.name} | ${err.message}`);
+  server.close(() => {
+    console.error("Shutting down...");
+    process.exit(1);
+  });
 });
-
-
-
-
-
 
 /*
 ________________________________________________________________________________________________
