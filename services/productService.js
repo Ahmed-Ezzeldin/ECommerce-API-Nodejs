ea65ea7fd93c48d21eaf9e70d3066e2910c923cd
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2023-08-14 13:31:13 +0300

    Upload Mix Of Product Images

--- services/productService.js
+++ services/productService.js
@@ -1,10 +1,62 @@
-const factory = require('./handlersFactory');
-const Product = require('../models/productModel');
+const multer = require("multer");
+const asyncHandler = require("express-async-handler");
+const sharp = require("sharp");
+const { v4: uuidv4 } = require("uuid");
+const factory = require("./handlersFactory");
+const Product = require("../models/productModel");
+const ApiError = require("../utils/apiError");
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = function (req, file, callback) {
+  // image/png
+  if (file.mimetype.startsWith("image")) {
+    callback(null, true);
+  } else {
+    callback(new ApiError("Only images allowed", 400), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadProductImages = upload.fields([
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 5 },
+]);
+
+exports.resizeProductImages = asyncHandler(async (req, res, next) => {
+  // Image processing for cover
+  if (req.files.imageCover) {
+    const imageCoverFileName = `product_${uuidv4()}_${Date.now()}-cover.jpeg`;
+    await sharp(req.files.imageCover[0].buffer)
+      //   .resize(600, 600)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(`uploads/products/${imageCoverFileName}`);
+    req.body.imageCover = imageCoverFileName;
+  }
+  // Image processing for images
+  if (req.files.images) {
+    req.body.images = [];
+    await Promise.all(
+      req.files.images.map(async (img, index) => {
+        const imageName = `product_${uuidv4()}_${Date.now()}-${index + 1}.jpeg`;
+        await sharp(img.buffer)
+          //   .resize(600, 600)
+          .toFormat("jpeg")
+          .jpeg({ quality: 90 })
+          .toFile(`uploads/products/${imageName}`);
+        req.body.images.push(imageName);
+      })
+    );
+    next();
+  }
+});
 
 // @desc    Get list of products
 // @route   GET /api/v1/products
 // @access  Public
-exports.getProducts = factory.getAll(Product, 'Products');
+exports.getProducts = factory.getAll(Product, "Products");
 
 // @desc    Get specific product by id
 // @route   GET /api/v1/products/:id
