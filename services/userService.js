b9b2e5ea81cb6b742d56bbb44b48998aea9486b3
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2023-08-15 23:44:50 +0300

    User Service & Route

---
+++ services/userService.js
@@ -0,0 +1,45 @@
+const sharp = require("sharp");
+const asyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
+const factory = require("./handlersFactory");
+const User = require("../models/userModel");
+const { uploadSingleImage } = require("../middlewares/uploadImageMiddleware");
+
+exports.uploadUserImage = uploadSingleImage("profileImg");
+
+exports.resizeImage = asyncHandler(async (req, res, next) => {
+  const filename = `user_${uuidv4()}_${Date.now()}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(600, 600)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`uploads/users/${filename}`);
+
+  req.body.profileImg = filename;
+  next();
+});
+
+// @desc    Get list of users
+// @route   GET /api/v1/users
+// @access  Private
+exports.getUsers = factory.getAll(User);
+
+// @desc    Get specific user by id
+// @route   GET /api/v1/users/:id
+// @access  Privates
+exports.getUser = factory.getOne(User);
+
+// @desc    Create user
+// @route   POST  /api/v1/users
+// @access  Private
+exports.createUser = factory.createOne(User);
+
+// @desc    Update specific user
+// @route   PUT /api/v1/users/:id
+// @access  Privates
+exports.updateUser = factory.updateOne(User);
+
+// @desc    Delete specific user
+// @route   DELETE /api/v1/users/:id
+// @access  Privates
+exports.deleteUser = factory.deleteOne(User);
